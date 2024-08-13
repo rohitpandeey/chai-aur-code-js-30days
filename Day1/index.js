2b55a47775ae8f78d660772f0b170e0c8e4e4f63
Rohit pandey <rohitpandey.8477@gmail.com>
2024-08-13 18:13:58 +0000

    day1 completed js

--- Day1/index.js
+++ Day1/index.js
@@ -1,1 +1,52 @@
-console.log("rohit")
+"use-strict"; // use all js code as per new std
+
+// activity1
+// Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+// Task 2: Declare a variable using let , assign it a string, and log the value to the Console
+
+var no =21
+let stringg= "Rohit"
+
+
+console.log(no);
+console.log(stringg);
+
+// Activity 2
+// Task 3: Declare a variable using const , assign it a boolean value, and log the value to the console.
+const a=true
+console.log(a);
+
+// Activity 3
+// Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator.
+const num=66
+const str="bawa"
+const obj={
+    name:"Himanshu",
+    rollno: 34
+}
+const arr= [1,2,3,"roror","dsd"]
+
+console.log(typeof(num))
+console.log(typeof(str))
+console.log(typeof(obj))
+console.log(typeof(arr))
+
+// Activity 4
+// • Task 5: Declare a variable using let , assign it an initial value, reassign a new value, and log both values to the console.
+let b=34
+console.log(b)
+b="rohit"
+console.log(b)
+
+// Activity 5
+// • Task 6: Try reassigning a variable declared with const and observe the error.
+const c= 12
+// c=3
+// console.log(c);
+
+//Gives TypeError: Assignment to constant variable.
+
+
+
+
+
